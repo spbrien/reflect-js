ab14ea53ef394cc05a3c8a85cdc3517dbe93cee7
Steven Brien <sbrien@hlkagency.com>
2017-06-28 13:08:11 -0500

    Nested simple relationships

--- relationships/index.js
+++ relationships/index.js
@@ -1,20 +1,20 @@
 module.exports = [
-  // {
-  //   resource: "wp_posts",
-  //   relatedTo: "wp_postmeta",
-  //   direct: true,
-  //   source_column: "ID",
-  //   target_column: "post_id"
-  // },
-  // {
-  //   resource: "wp_posts",
-  //   relatedTo: "wp_posts",
-  //   direct: false,
-  //   through: "wp_postmeta",
-  //   source_column: "ID",
-  //   intermediate_target_column: "post_id",
-  //   intermediate_source_column: "meta_id",
-  //   target_column: "ID",
-  //   as: "wp_posts"
-  // },
+  {
+    resource: "wp_posts",
+    relatedTo: "wp_postmeta",
+    direct: true,
+    source_column: "ID",
+    target_column: "post_id"
+  },
+  {
+    resource: "wp_posts",
+    relatedTo: "wp_posts",
+    direct: false,
+    through: "wp_postmeta",
+    source_column: "ID",
+    intermediate_target_column: "post_id",
+    intermediate_source_column: "meta_id",
+    target_column: "ID",
+    as: "wp_posts"
+  },
 ]
