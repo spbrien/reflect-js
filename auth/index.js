cace3730f91d46b13473c0ec298b37c5e03dcd93
Steven Brien <sbrien@hlkagency.com>
2017-07-07 10:02:41 -0500

    Oops disable auth based on config option

--- auth/index.js
+++ auth/index.js
@@ -4,9 +4,5 @@ const config = require('../config')
 module.exports = {
   disabled: config.authentication, // Disable all auth
   GET: {
-    employees: {
-      authorizedUsers: ['admin'],
-      authorizedRoles: ['admin']
-    }
   },
 }
--- relationships/index.js
+++ relationships/index.js
@@ -6,15 +6,15 @@ module.exports = [
   //   source_column: "ID",
   //   target_column: "post_id"
   // },
-  {
-    resource: "employees",
-    relatedTo: "departments",
-    direct: false,
-    through: "dept_emp",
-    source_column: "emp_no",
-    intermediate_target_column: "dept_no",
-    intermediate_source_column: "emp_no",
-    target_column: "dept_no",
-    as: "wp_posts"
-  },
+  // {
+  //   resource: "employees",
+  //   relatedTo: "departments",
+  //   direct: false,
+  //   through: "dept_emp",
+  //   source_column: "emp_no",
+  //   intermediate_target_column: "dept_no",
+  //   intermediate_source_column: "emp_no",
+  //   target_column: "dept_no",
+  //   as: "wp_posts"
+  // },
 ]
